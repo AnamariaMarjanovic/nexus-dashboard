ddeb980d7f836c155883663292772450d0d94e53
Anamaria Orsulic <102580384+AnamariaMarjanovic@users.noreply.github.com>
2026-09-03 15:44:22 +0200

    feat: initial Nx workspace with native-federation setup for shell and analytics remote

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'analytics',
+        loadComponent: () => loadRemoteModule('analytics', './Component').then(m => m.App)
+    }
+];
